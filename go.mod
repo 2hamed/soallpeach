96c4cff1d96aec475dad817299084c007d537dde
Hamed Momeni <hamed.ma7@gmail.com>
2020-04-16 14:15:33 +0430

    reimplemented in rust

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/2hamed/soallpeach/prime
-
-go 1.13
---
+++ prime/src/main.rs
@@ -0,0 +1,30 @@
+use std::io::BufRead;
+use std::{env, fs, io};
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    match fs::File::open(file_path) {
+        Err(e) => panic!(e),
+        Ok(file) => {
+            let f = io::BufReader::new(file);
+            for line in f.lines() {
+                match line {
+                    Err(e) => panic!(e),
+                    Ok(line) => match line.parse::<u64>() {
+                        Err(e) => panic!(e),
+                        Ok(n) => println!("{}", isPrime(n)),
+                    },
+                }
+            }
+        }
+    }
+}
+fn isPrime(n: u64) -> u8 {
+    let sqrt = (n as f64).sqrt() as u64;
+    for i in 2..sqrt {
+        if n % i == 0 {
+            return 0;
+        }
+    }
+    return 1;
+}
