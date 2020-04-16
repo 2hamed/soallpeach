0b17a757b376a395b0044fa509d61c9d4db8ef2d
Hamed Momeni <hamed.ma7@gmail.com>
2020-04-16 21:03:56 +0430

    optimized prime

--- prime/src/main.rs
+++ prime/src/main.rs
@@ -1,28 +1,34 @@
+use std::collections::HashMap;
 use std::io::BufRead;
 use std::{env, fs, io};
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
-    match fs::File::open(file_path) {
+    let reader = match fs::File::open(file_path) {
         Err(e) => panic!(e),
-        Ok(file) => {
-            let f = io::BufReader::new(file);
-            for line in f.lines() {
-                match line {
-                    Err(e) => panic!(e),
-                    Ok(line) => match line.parse::<u64>() {
-                        Err(e) => panic!(e),
-                        Ok(n) => println!("{}", is_prime(n)),
-                    },
-                }
-            }
+        Ok(file) => io::BufReader::new(file),
+    };
+    let mut map: HashMap<u64, u8> = HashMap::new();
+    for line in reader.lines() {
+        match line {
+            Err(e) => panic!(e),
+            Ok(line) => match line.parse::<u64>() {
+                Err(e) => panic!(e),
+                Ok(n) => println!("{}", is_prime(n, &mut map)),
+            },
         }
     }
 }
-fn is_prime(n: u64) -> u8 {
+fn is_prime(n: u64, map: &mut HashMap<u64, u8>) -> u8 {
+    if map.contains_key(&n) {
+        return 0;
+    }
+
     let sqrt = (n as f64).sqrt() as u64;
     for i in 2..sqrt {
         if n % i == 0 {
+            map.insert(i, 0);
             return 0;
         }
     }
