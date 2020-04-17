ef4506c4fede65c70f314b428d340cfa0d229170
Hamed Momeni <hamed.ma7@gmail.com>
2020-04-18 00:18:24 +0430

    implmented sieve of atkin

--- prime/src/main.rs
+++ prime/src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::io::BufRead;
 use std::{env, fs, io};
 
@@ -9,37 +8,62 @@ fn main() {
         Err(e) => panic!(e),
         Ok(file) => io::BufReader::new(file),
     };
-    let mut map: HashMap<u64, u8> = HashMap::new();
     for line in reader.lines() {
         match line {
             Err(e) => panic!(e),
-            Ok(line) => match line.parse::<u64>() {
+            Ok(line) => match line.parse::<i64>() {
                 Err(e) => panic!(e),
-                Ok(n) => println!("{}", is_prime(n, &mut map)),
+                Ok(n) => println!(
+                    "{}",
+                    if *sieve_of_atkin(n).last().unwrap() {
+                        1
+                    } else {
+                        0
+                    }
+                ),
             },
         }
     }
 }
-fn is_prime(n: u64, map: &mut HashMap<u64, u8>) -> u8 {
-    if n <= 1 {
-        return 0;
-    }
 
-    if n == 2 || n == 3 {
-        return 1;
-    }
+fn sieve_of_atkin(limit: i64) -> Vec<bool> {
+    let mut sieve: Vec<bool> = (0..(limit + 1))
+        .map(|i| {
+            if i == 2 || i == 3 || i == 5 {
+                return true;
+            }
+            return false;
+        })
+        .collect();
 
-    if map.contains_key(&n) {
-        return map[&n];
-    }
-
-    let sqrt = (n as f64).sqrt() as u64;
-    for i in 2..sqrt {
-        if n % i == 0 {
-            map.insert(n, 0);
-            return 0;
+    let mut x = 1;
+    while x * x < limit {
+        let mut y = 1;
+        while y * y < limit {
+            let n = (4 * x * x) + (y * y);
+            if n <= limit && (n % 12 == 1 || n % 12 == 5) {
+                sieve[n as usize] ^= true;
+            }
+            let n = (3 * x * x) + (y * y);
+            if n <= limit && n % 12 == 7 {
+                sieve[n as usize] ^= true;
+            }
+            let n = (3 * x * x) - (y * y);
+            if x > y && n <= limit && n % 12 == 11 {
+                sieve[n as usize] ^= true;
+            }
+            y += 1;
         }
+        x += 1;
     }
-    map.insert(n, 1);
-    return 1;
+    let mut r = 5;
+    while r * r < limit {
+        if sieve[r as usize] {
+            for i in (r * r..limit).step_by((r * r) as usize) {
+                sieve[i as usize] = false;
+            }
+        }
+        r += 1;
+    }
+    sieve
 }
